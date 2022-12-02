4c7b554a4813dc0e224431fb86d0224dd31d08aa
BrennanCoats <brennan.coats@gmail.com>
2022-12-01 23:26:04 -0700

    made the API work!

--- Brennans_assets/BCscript.js
+++ Brennans_assets/BCscript.js
@@ -1,29 +1,22 @@
 var fetchButton = document.getElementById('submit');
-var artists = document.getElementById('descriptionBox');
-var searchBar = document.getElementById('searchArtist').value;
 
 
 function getApi(event) {
     event.preventDefault();
-    console.log(artists);
-    console.log(searchBar);
-    var requestUrl = ` http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${searchBar}&api_key=6eb7995f9da6e507011787533014528f&format=json`;
-    console.log(requestUrl);
+    var searchBar = document.getElementById('searchArtist').value;
+    var requestUrl = ` http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&format=json&artist=${searchBar}&api_key=6eb7995f9da6e507011787533014528f`;
+    
     fetch(requestUrl)
-      .then(function (response) {
-        console.log(artists);
-        return response.json();
-        
+    .then((response) => {
+       return response.json();
       })
-      .then(function (data) {
-        for (var i = 0; i < data.length; i++) {
-          var listItem = document.createElement('span');
-          listItem.textContent = data[i].html_url;
-          artists.appendChild(listItem);
-        }
-      });
-  }
-
+      .then(function(data){
+        var obj = JSON.parse(JSON.stringify(data));
+        var description = document.getElementById('descriptionBox');
+        description.textContent = obj.artist.bio.summary;
+      })
+  
+    }
 
 
 
