db2e2c5189f3dae2ca96c632ab7b99da49e62a8e
BrennanCoats <brennan.coats@gmail.com>
2022-12-05 21:51:02 -0700

    added buttons for links

--- Sada_assets/sada.js
+++ Sada_assets/sada.js
@@ -1,21 +1,21 @@
 var fetchButton = document.getElementById('submit');
 
 
-function getApi(event) {
-  event.preventDefault();
-  var searchBar = document.getElementById('searchArtist').value;
-  $('input[name="searchArtist"]').val('');
-  var requestUrl = ` http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&format=json&artist=${searchBar}&api_key=6eb7995f9da6e507011787533014528f`;
+//function getApi(event) {
+//  event.preventDefault();
+//  var searchBar = document.getElementById('searchArtist').value;
+//  $('input[name="searchArtist"]').val('');
+//  var requestUrl = ` http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&format=json&artist=${searchBar}&api_key=6eb7995f9da6e507011787533014528f`;
 
-  fetch(requestUrl)
-    .then((response) => {
-      return response.json();
-    })
-    .then(function (data) {
-      var obj = JSON.parse(JSON.stringify(data));
-      var description = document.getElementById('descriptionBox');
-      description.textContent = obj.artist.bio.summary;
-    })
+//  fetch(requestUrl)
+//    .then((response) => {
+//      return response.json();
+//    })
+//    .then(function (data) {
+//      var obj = JSON.parse(JSON.stringify(data));
+//      var description = document.getElementById('descriptionBox');
+//      description.textContent = obj.artist.bio.summary;
+//    })
 
 
   // This function saves the users search history  
