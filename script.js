3ad6b57a88f43db5c4200d6e9537f1d2f34b4bd5
patrick <roosterruf@gmail.com>
2022-12-07 21:49:36 -0700

    resolving merge conflicts

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var fetchButton = document.getElementById('submit');
-<<<<<<< Updated upstream
 var wikiLink = document.getElementById('wikiLink');
 var youtubeLink = document.getElementById('youtubeLink');
 wikiLink.style.visibility='hidden';
@@ -48,38 +47,7 @@ var linkBoxTitle = document.getElementById('linkBoxTitle');
     searchHistory.append(searchHistoryE1)
 
 
-=======
-var apiKey = "6eb7995f9da6e507011787533014528";
 
-
-
-function getApi(event) {
-  event.preventDefault();
-  fetchApi();
-  var searchBar = document.getElementById('searchArtist').value;
-  var requestUrl = ` http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&format=json&artist=${searchBar}&api_key=6eb7995f9da6e507011787533014528f`;
-  
-  fetch(requestUrl)
-  .then((response) => {
-    return response.json();
-  })
-  .then(function (data) {
-    var obj = JSON.parse(JSON.stringify(data));
-      var description = document.getElementById('descriptionBox');
-      description.textContent = obj.artist.bio.summary;
-    })
-    
-    
-    // This function saves the users search history  
-    function saveHistory(event) {
-      // event.preventDefault();
-      var searchHistory = $('#searchHistory')
-      var searchHistoryE1 = $('<button class="searchHistoryResult">')
-      searchHistoryE1.text(searchBar)
-      searchHistory.append(searchHistoryE1)
-      
-      
->>>>>>> Stashed changes
   }
   saveHistory()
 
