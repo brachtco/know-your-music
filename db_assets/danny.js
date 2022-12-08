b2fd57363784209313082867596c169dca17de15
brachtco <brachtdanny@gmail.com>
2022-12-07 18:20:42 -0700

    Add functions and fetch code Update css

--- db_assets/danny.js
+++ db_assets/danny.js
@@ -1,28 +1,40 @@
-var apiKey = "6eb7995f9da6e507011787533014528f";
-
-console.log("hello");
-
+var apiKey = "6eb7995f9da6e507011787533014528";
 var fetchButton = document.getElementById("submit");
 
-//get info from API and load decription in box.
-function getApi(event) {
-  event.preventDefault();
-  var searchBar = document.getElementById("").value;
-  //var requestUrl =  'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&format=json&artist=${searchBar}&api_key=6eb7995f9da6e507011787533014528f`;
-  // var genreUrl = 'http://musicbrainz.org/ws/2/genre/f66d7266-eb3d-4ef3-b4d8-b7cd992f918b';
+function showAlbum(artist) {
+  var url = `http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${artist}&api_key=6eb7995f9da6e507011787533014528f&format=json`;
+  var image = document.getElementById("imageCard");
 
-  var imageUrl =
-    'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist${searchBar}=&api_key=6eb7995f9da6e507011787533014528f&format=json';
-
-  fetch(imageUrl)
-    .then((response) => {
-      return response.json();
-    })
-    .then(function (data) {
-      var obj = JSON.parse(JSON.stringify(data));
-      var image = document.getElementById("imageCard");
-      image.textContent = obj.artist.imagesize("small");
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const imageUrl = data.topalbums.album[0].image.pop()["#text"];
+      image.innerHTML = `<img src="${imageUrl}" />`;
     });
 }
 
-fetchButton.addEventListener("click", getApi);
+function showInfo(artist) {
+  var url = `http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artist}&api_key=6eb7995f9da6e507011787533014528f&format=json`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const genres = data.artist.tags.tag.map((genre) => genre.name).join(", ");
+      document.getElementById("genreCard").textContent = genres;
+    });
+}
+
+//get info from API and load decription in box.
+function fetchApi(event) {
+  event.preventDefault();
+  var artist = document.getElementById("searchArtist").value;
+  showAlbum(artist);
+  showInfo(artist);
+}
+
+fetchButton.addEventListener("click", fetchApi);
+
+
+// var genreUrl = 'http://musicbrainz.org/ws/2/genre/f66d7266-eb3d-4ef3-b4d8-b7cd992f918';
+
+//var genreUrl2 = 'https://musicbrainz.org/ws/2/release-group/3bd76d40-7f0e-36b7-9348-91a33afee20e?inc=genres+user-genres&fmt=json';
