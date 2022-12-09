8588d9c0ad317f31eaeb1a2a42ea9cab09250394
BrennanCoats <brennan.coats@gmail.com>
2022-12-08 19:37:46 -0700

    changes to readme file

--- Brennans_assets/BCscript.js
+++ Brennans_assets/BCscript.js
@@ -1,49 +1,72 @@
-var fetchButton = document.getElementById('submit');
-var wikiLink = document.getElementById('wikiLink');
-var youtubeLink = document.getElementById('youtubeLink');
-wikiLink.style.visibility='hidden';
-youtubeLink.style.visibility='hidden';
+// var fetchButton = document.getElementById('submit');
+// var wikiLink = document.getElementById('wikiLink');
+// var youtubeLink = document.getElementById('youtubeLink');
+// wikiLink.style.visibility='hidden';
+// youtubeLink.style.visibility='hidden';
 
-//get info from API and load decription in box.
-function getApi(event) {
-    event.preventDefault();
+// //get info from API and load decription in box.
+// function getApi(event) {
+//     event.preventDefault();
     
-    var searchBar = document.getElementById('searchArtist').value;
-    var linkBoxTitle = document.getElementById('linkBoxTitle');
+//     var searchBar = document.getElementById('searchArtist').value;
+//     var linkBoxTitle = document.getElementById('linkBoxTitle');
     
-    linkBoxTitle.textContent = searchBar;
-    linkBoxTitle.dataset.lastSearch = searchBar;
-    $('input[name="searchArtist"]').val('');
+//     linkBoxTitle.textContent = searchBar;
+//     linkBoxTitle.dataset.lastSearch = searchBar;
+//     $('input[name="searchArtist"]').val('');
     
-    youtubeLink.addEventListener('click', function(){
-      travelToVideo(linkBoxTitle.dataset.lastSearch);
-      });
+//     youtubeLink.addEventListener('click', function(){
+//       travelToVideo(linkBoxTitle.dataset.lastSearch);
+//       });
 
-    wikiLink.addEventListener('click',function(){
-      travelToLink(linkBoxTitle.dataset.lastSearch);
-      });
+//     wikiLink.addEventListener('click',function(){
+//       travelToLink(linkBoxTitle.dataset.lastSearch);
+//       });
 
-    var requestUrl = ` http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&format=json&artist=${searchBar}&api_key=6eb7995f9da6e507011787533014528f`;
-    fetch(requestUrl)
+//     var requestUrl = ` http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&format=json&artist=${searchBar}&api_key=6eb7995f9da6e507011787533014528f`;
+//     fetch(requestUrl)
+//     .then((response) => {
+//        return response.json();
+//       })
+//       .then(function(data){
+//         var obj = JSON.parse(JSON.stringify(data));
+//         var description = document.getElementById('descriptionBox');
+//         description.textContent = obj.artist.bio.summary;
+//         wikiLink.style.visibility='visible';
+//         youtubeLink.style.visibility='visible';
+//       })
+//     }
+
+// function travelToLink (searchBar) {
+//     window.location.href= `https://en.wikipedia.org/wiki/${searchBar}`;
+//     }
+
+// function travelToVideo (searchBar) {
+//     window.location.href= `https://www.youtube.com/results?search_query=${searchBar}`;
+//     }
+
+//   fetchButton.addEventListener('click', getApi);
+
+function getLinksApi () {
+
+  linksUrl = `https://tastedive.com/api/similar?{query}={value}&k=445472-KnowYour-SHRH2F95`;
+
+  fetch(linksUrl)
     .then((response) => {
+      console.log(response);
        return response.json();
       })
-      .then(function(data){
-        var obj = JSON.parse(JSON.stringify(data));
-        var description = document.getElementById('descriptionBox');
-        description.textContent = obj.artist.bio.summary;
-        wikiLink.style.visibility='visible';
-        youtubeLink.style.visibility='visible';
-      })
-    }
+      // .then(function(data){
+      //   var obj = JSON.parse(JSON.stringify(data));
+      //   var description = document.getElementById('descriptionBox');
+      //   description.textContent = obj.artist.bio.summary;
+      //   wikiLink.style.visibility='visible';
+      //   youtubeLink.style.visibility='visible';
+      // })
 
-function travelToLink (searchBar) {
-    window.location.href= `https://en.wikipedia.org/wiki/${searchBar}`;
-    }
 
-function travelToVideo (searchBar) {
-    window.location.href= `https://www.youtube.com/results?search_query=${searchBar}`;
-    }
 
-  fetchButton.addEventListener('click', getApi);
+}
+
+
 
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # project01
+Know Your Music!
+
+Know Your Music is a website that allows you to search up any music artist and recieve a bunch of information about them.
+
+When you type an artist name into the input field and click submit
+Then you will recieve information describing the artist'; Top tracks, Genre of music, image of the artist' most relevant    album, and some links to wikipedia and youtube.
+When an an artist is searched,
+Then it is displayed in the search history section.
+When you click on the saved search history buttons,
+Then it searches that artist again.
+
+[Here is a link to our deployed application.]()
+
+[Here you can view a screenshot of the application]()
