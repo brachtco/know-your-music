77e4bbe4255269fcd435433c312a57607f9dff1c
brachtco <brachtdanny@gmail.com>
2022-12-12 10:22:45 -0700

    Update comments in js

--- db_assets/danny.js
+++ db_assets/danny.js
@@ -1,6 +1,7 @@
 var apiKey = "6eb7995f9da6e507011787533014528";
 var fetchButton = document.getElementById("submit");
 
+//Use the pop method to show the album image
 function showAlbum(artist) {
   var url = `http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${artist}&api_key=6eb7995f9da6e507011787533014528f&format=json`;
   var image = document.getElementById("imageCard");
@@ -8,11 +9,12 @@ function showAlbum(artist) {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      const imageUrl =data.topalbums.album[0].image.pop()["#text"];
+      const imageUrl = data.topalbums.album[0].image.pop()["#text"];
       image.innerHTML = `<img src="${imageUrl}" />`;
     });
 }
 
+//Use the map method to print the genres to tyhe scre
 function showInfo(artist) {
   var url = `http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artist}&api_key=6eb7995f9da6e507011787533014528f&format=json`;
 
@@ -24,7 +26,7 @@ function showInfo(artist) {
     });
 }
 
-//get info from API and load decription in box.
+//fetch function the input value and call the functions to show the ablum image and the genres
 function fetchApi(event) {
   event.preventDefault();
   var artist = document.getElementById("searchArtist").value;
@@ -33,7 +35,6 @@ function fetchApi(event) {
 }
 
 fetchButton.addEventListener("click", fetchApi);
-
 
 // var genreUrl = 'http://musicbrainz.org/ws/2/genre/f66d7266-eb3d-4ef3-b4d8-b7cd992f918';
 
